47b6f11500d6dcb58a4d007ad3f3e3ffb6f854db
RadicalGarlic <nobody@nowhere>
2023-12-08 16:52:21 -0800

    Directory traversal

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
 import * as fs from 'node:fs';
+import * as fsPromises from 'node:fs/promises';
 
 async function main() {
-  console.log('main');
+  if (process.argv.length < 3) {
+    console.log('Specify directory path');
+  }
+
+  const entries: fs.Dirent[] = await fsPromises.readdir(process.argv[2], {
+    withFileTypes: true,
+    recursive: true,
+  });
+
+  entries.forEach((entry: fs.Dirent) => {
+    if (entry.isFile()) {
+      console.log(`${entry.path}/${entry.name}`);
+    }
+  });
 }
 
 main();
